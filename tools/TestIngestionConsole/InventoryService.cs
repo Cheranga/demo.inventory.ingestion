b6c188d8edc747dc0f14cefb15f844b05926cb8e
Cheranga Hatangala <cheranga@gmail.com>
2022-12-11 18:51:08 +1100

    chore: adding a console app to load test the function app [ci]

---
+++ tools/TestIngestionConsole/InventoryService.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace TestIngestionConsole;
+
+public record SendInventoryEventRequest(string CorrelationId, string FileName);
+
+public interface IInventoryService
+{
+    Task<bool> SendInventoryEvent(SendInventoryEventRequest request);
+}
+
+public class InventoryService : IInventoryService
+{
+    private readonly HttpClient _client;
+
+    public InventoryService(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<bool> SendInventoryEvent(SendInventoryEventRequest request)
+    {
+        var requestContent = JsonConvert.SerializeObject(request);
+        var response = await _client.SendAsync(new HttpRequestMessage(HttpMethod.Post,
+            new Uri("[hosted URI]/api/inventory"))
+        {
+            Content = new StringContent(requestContent)
+        });
+
+        return response.IsSuccessStatusCode;
+    }
+}
