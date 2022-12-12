627f5ee595e1eccd05469b892e8e5a0028330241
Cheranga Hatangala <cheranga@gmail.com>
2022-12-12 23:03:12 +1100

    fix: fixing configuration reads for queue trigger [ci]

--- src/Demo.Inventory.Ingestion.Functions/Features/ReadInventoryChanges/ReadInventoryChangesClientFunction.cs
+++ src/Demo.Inventory.Ingestion.Functions/Features/ReadInventoryChanges/ReadInventoryChangesClientFunction.cs
@@ -7,7 +7,7 @@ namespace Demo.Inventory.Ingestion.Functions.Features.ReadInventoryChanges;
 public class ReadInventoryChangesClientFunction
 {
     [FunctionName(nameof(ReadInventoryChangesClientFunction))]
-    public async Task RunAsync([QueueTrigger("%AcceptInventorySettings::Queue%")] string message)
+    public async Task RunAsync([QueueTrigger("%AcceptInventorySettings:Queue%")] string message)
     {
         await Task.Delay(TimeSpan.FromSeconds(2));
     }
