e86880a4abbf08e18144076cd62953bdda5143b0
Cheranga Hatangala <cheranga@gmail.com>
2022-12-12 22:36:13 +1100

    chore: initiating durable function setup [ci]

---
+++ src/Demo.Inventory.Ingestion.Functions/Features/ProcessInventoryChanges/ReadInventoryChangesClientFunction.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System;
+using Microsoft.Azure.WebJobs;
+
+namespace Demo.Inventory.Ingestion.Functions.Features.ProcessInventoryChanges;
+
+public class ReadInventoryChangesClientFunction
+{
+    [FunctionName(nameof(ReadInventoryChangesClientFunction))]
+    public async Task RunAsync([QueueTrigger("%AcceptInventorySettings::Queue%")] string message)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(2));
+    }
+}
